5eaba2b08814ca5b3de70dda278d610d626e3a10
Captain Unknown <captainunknown7@gmail.com>
2025-03-28 17:47:52 +0500

    Initial commit

---
+++ src/supply-chain.ts
@@ -0,0 +1,123 @@
+import {
+  Approval as ApprovalEvent,
+  ApprovalForAll as ApprovalForAllEvent,
+  BatchCreated as BatchCreatedEvent,
+  BatchMetadataUpdate as BatchMetadataUpdateEvent,
+  BatchStatusUpdated as BatchStatusUpdatedEvent,
+  MetadataUpdate as MetadataUpdateEvent,
+  Transfer as TransferEvent
+} from "../generated/SupplyChain/SupplyChain"
+import {
+  Approval,
+  ApprovalForAll,
+  BatchCreated,
+  BatchMetadataUpdate,
+  BatchStatusUpdated,
+  MetadataUpdate,
+  Transfer
+} from "../generated/schema"
+
+export function handleApproval(event: ApprovalEvent): void {
+  let entity = new Approval(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.approved = event.params.approved
+  entity.tokenId = event.params.tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleApprovalForAll(event: ApprovalForAllEvent): void {
+  let entity = new ApprovalForAll(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.operator = event.params.operator
+  entity.approved = event.params.approved
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleBatchCreated(event: BatchCreatedEvent): void {
+  let entity = new BatchCreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.batchId = event.params.batchId
+  entity.hash = event.params.hash
+  entity.timestamp = event.params.timestamp
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleBatchMetadataUpdate(
+  event: BatchMetadataUpdateEvent
+): void {
+  let entity = new BatchMetadataUpdate(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity._fromTokenId = event.params._fromTokenId
+  entity._toTokenId = event.params._toTokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleBatchStatusUpdated(event: BatchStatusUpdatedEvent): void {
+  let entity = new BatchStatusUpdated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.batchId = event.params.batchId
+  entity.state = event.params.state
+  entity.hash = event.params.hash
+  entity.timestamp = event.params.timestamp
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleMetadataUpdate(event: MetadataUpdateEvent): void {
+  let entity = new MetadataUpdate(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity._tokenId = event.params._tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new Transfer(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.tokenId = event.params.tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
---
+++ tests/supply-chain-utils.ts
@@ -0,0 +1,185 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  Approval,
+  ApprovalForAll,
+  BatchCreated,
+  BatchMetadataUpdate,
+  BatchStatusUpdated,
+  MetadataUpdate,
+  Transfer
+} from "../generated/SupplyChain/SupplyChain"
+
+export function createApprovalEvent(
+  owner: Address,
+  approved: Address,
+  tokenId: BigInt
+): Approval {
+  let approvalEvent = changetype<Approval>(newMockEvent())
+
+  approvalEvent.parameters = new Array()
+
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("approved", ethereum.Value.fromAddress(approved))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return approvalEvent
+}
+
+export function createApprovalForAllEvent(
+  owner: Address,
+  operator: Address,
+  approved: boolean
+): ApprovalForAll {
+  let approvalForAllEvent = changetype<ApprovalForAll>(newMockEvent())
+
+  approvalForAllEvent.parameters = new Array()
+
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("operator", ethereum.Value.fromAddress(operator))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("approved", ethereum.Value.fromBoolean(approved))
+  )
+
+  return approvalForAllEvent
+}
+
+export function createBatchCreatedEvent(
+  batchId: BigInt,
+  hash: string,
+  timestamp: BigInt
+): BatchCreated {
+  let batchCreatedEvent = changetype<BatchCreated>(newMockEvent())
+
+  batchCreatedEvent.parameters = new Array()
+
+  batchCreatedEvent.parameters.push(
+    new ethereum.EventParam(
+      "batchId",
+      ethereum.Value.fromUnsignedBigInt(batchId)
+    )
+  )
+  batchCreatedEvent.parameters.push(
+    new ethereum.EventParam("hash", ethereum.Value.fromString(hash))
+  )
+  batchCreatedEvent.parameters.push(
+    new ethereum.EventParam(
+      "timestamp",
+      ethereum.Value.fromUnsignedBigInt(timestamp)
+    )
+  )
+
+  return batchCreatedEvent
+}
+
+export function createBatchMetadataUpdateEvent(
+  _fromTokenId: BigInt,
+  _toTokenId: BigInt
+): BatchMetadataUpdate {
+  let batchMetadataUpdateEvent = changetype<BatchMetadataUpdate>(newMockEvent())
+
+  batchMetadataUpdateEvent.parameters = new Array()
+
+  batchMetadataUpdateEvent.parameters.push(
+    new ethereum.EventParam(
+      "_fromTokenId",
+      ethereum.Value.fromUnsignedBigInt(_fromTokenId)
+    )
+  )
+  batchMetadataUpdateEvent.parameters.push(
+    new ethereum.EventParam(
+      "_toTokenId",
+      ethereum.Value.fromUnsignedBigInt(_toTokenId)
+    )
+  )
+
+  return batchMetadataUpdateEvent
+}
+
+export function createBatchStatusUpdatedEvent(
+  batchId: BigInt,
+  state: i32,
+  hash: string,
+  timestamp: BigInt
+): BatchStatusUpdated {
+  let batchStatusUpdatedEvent = changetype<BatchStatusUpdated>(newMockEvent())
+
+  batchStatusUpdatedEvent.parameters = new Array()
+
+  batchStatusUpdatedEvent.parameters.push(
+    new ethereum.EventParam(
+      "batchId",
+      ethereum.Value.fromUnsignedBigInt(batchId)
+    )
+  )
+  batchStatusUpdatedEvent.parameters.push(
+    new ethereum.EventParam(
+      "state",
+      ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(state))
+    )
+  )
+  batchStatusUpdatedEvent.parameters.push(
+    new ethereum.EventParam("hash", ethereum.Value.fromString(hash))
+  )
+  batchStatusUpdatedEvent.parameters.push(
+    new ethereum.EventParam(
+      "timestamp",
+      ethereum.Value.fromUnsignedBigInt(timestamp)
+    )
+  )
+
+  return batchStatusUpdatedEvent
+}
+
+export function createMetadataUpdateEvent(_tokenId: BigInt): MetadataUpdate {
+  let metadataUpdateEvent = changetype<MetadataUpdate>(newMockEvent())
+
+  metadataUpdateEvent.parameters = new Array()
+
+  metadataUpdateEvent.parameters.push(
+    new ethereum.EventParam(
+      "_tokenId",
+      ethereum.Value.fromUnsignedBigInt(_tokenId)
+    )
+  )
+
+  return metadataUpdateEvent
+}
+
+export function createTransferEvent(
+  from: Address,
+  to: Address,
+  tokenId: BigInt
+): Transfer {
+  let transferEvent = changetype<Transfer>(newMockEvent())
+
+  transferEvent.parameters = new Array()
+
+  transferEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return transferEvent
+}
